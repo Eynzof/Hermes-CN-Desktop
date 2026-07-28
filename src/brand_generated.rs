eac5b8806446eeca37019b92c936e418e15d0460
nevermorewish <nevermorewish@hotmail.com>
2026-07-28 17:00:49 +0800

    fix: align desktop update feed

--- src/brand_generated.rs
+++ src/brand_generated.rs
@@ -14,5 +14,5 @@ pub const BRAND_DATA_DIR_NAME: &str = ".huanxinghermes";
 pub const BRAND_WINDOW_TITLE: &str = "HermesAgent 桌面版";
 pub const BRAND_HOMEPAGE: &str = "https://api.huanxing.ai";
 pub const BRAND_UPDATE_MANIFEST_URL: &str =
-    "https://huanxing.tos-cn-beijing.volces.com/package/hermesagent/huanxinghermes/latest.json";
+    "https://huanxing.tos-cn-beijing.volces.com/package/hermesagent/huanxingcomhermes/latest.json";
 pub const BRAND_UPDATE_DOWNLOAD_URL: &str = "https://huanxingapi.com/hermes-client";
